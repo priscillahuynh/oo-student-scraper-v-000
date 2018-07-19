a0a99e7a7528dbc2322fba1d8880dc4a8b34bdc0
Priscilla H <priscillamhuynh@gmail.com>
2018-07-19 08:12:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
-# @@all = []
+@@all = []
 
   def self.scrape_index_page(index_url)
     learn = Nokogiri::HTML(open(index_url))
@@ -18,9 +18,23 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    student_hash = {}
-    learn = Nokogiri::HTML(open(index_url))
+    learn = Nokogiri::HTML(open(profile_url))
+    student = {}
 
+    learn.css("social-icon-container a").each do |link|
+    href = link.attr("href")
+      if href.include?("twitter")
+        student[:twitter] = href
+      elsif href.include?("linkedin")
+        student[:linkedin] = href
+      elsif href.include?("github")
+        student[:github] = href
+      else
+        student[:blog] = href
+      end
+    end
+      student[:profile_quote] = learn.css(".vitals-text-container .profile-quote").text
+      student[:bio] = learn.css(".details-container .description-holder p").text
+      # binding.pry
   end
-
 end
