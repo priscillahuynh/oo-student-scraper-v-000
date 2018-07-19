d4ed77c6b129ed0a56c2df02e7dca5861f4be017
Priscilla H <priscillamhuynh@gmail.com>
2018-07-19 10:39:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each_pair {|key,value|
-    self.send (("#{key}=", value))}
+    self.send (("#{key}="), value)}
   end
 
   def self.all
